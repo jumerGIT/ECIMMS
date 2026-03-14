6de2e734434dbab6bca7c751e4f880bd4c44514d
jumerGIT <126186825+jumerGIT@users.noreply.github.com>
2026-03-14 18:09:46 +0800

    Update ALLOWED_HOSTS for production deployment

--- hms_project/settings.py
+++ hms_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hms-replace-this-befo
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'https://ecimms.onrender.com').split(',')
 
 # CSRF settings for HTTPS deployment
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if os.environ.get('CSRF_TRUSTED_ORIGINS') else []
