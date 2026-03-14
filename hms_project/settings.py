c2ba7816cf545fa9b1246bfb477789f9de9c2b22
Jumer Ofamin <JumerO1@verifone.com>
2026-03-14 16:49:49 +0800

    Allowed host

--- hms_project/settings.py
+++ hms_project/settings.py
@@ -16,10 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hms-replace-this-befo
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
-# CSRF settings for HTTPS deployment
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if os.environ.get('CSRF_TRUSTED_ORIGINS') else []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
